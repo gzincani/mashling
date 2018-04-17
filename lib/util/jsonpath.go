667c6b7fd6b1b49912530aa73bbcaed5dfff71cf
nareshkumarthota <nthota@tibco.com>
2018-04-17 11:27:12 +0530

    jsonpath changes initial work around

--- lib/util/jsonpath.go
+++ lib/util/jsonpath.go
@@ -6,40 +6,59 @@
 package util
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
-	"strings"
 
 	"github.com/TIBCOSoftware/flogo-lib/logger"
-	"github.com/jeffreybozek/jsonpath"
-	"github.com/pkg/errors"
+	"github.com/oliveagle/jsonpath"
+	// "github.com/TIBCOSoftware/flogo-lib/logger"
+	// "github.com/jeffreybozek/jsonpath"
+	// "github.com/pkg/errors"
 )
 
 var newLogger = logger.GetLogger("jsonpath-eval")
 
+// func JsonPathEval(jsonData string, expression string) (*string, error) {
+// 	paths, err := jsonpath.ParsePaths(expression)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	newLogger.Debugf("jsonpath expression is [%v], data is [%v]", expression, jsonData)
+
+// 	eval, err := jsonpath.EvalPathsInBytes([]byte(jsonData), paths)
+// 	if err != nil {
+// 		newLogger.Infof("unable to evaluate jsonpath expression, error is [%v]", err)
+// 		return nil, err
+// 	}
+
+// 	for {
+// 		if result, ok := eval.Next(); ok {
+// 			//return after the first match
+// 			value := string(result.Value) //The value obtained will be encased in double quote characters, e.g. "USA" when the value happens to be USA.
+// 			//Trim the double quote suffix and prefix
+// 			value = strings.TrimPrefix(value, "\"")
+// 			value = strings.TrimSuffix(value, "\"")
+// 			newLogger.Debugf("jsonpath [%v] evaluated to value [%v]", expression, value)
+// 			return &value, nil // true -> show keys in pretty string
+// 		} else {
+// 			return nil, errors.New(fmt.Sprintf("Error evaluating jsonpath expression[%v] on content [%v]", expression, jsonData))
+// 		}
+// 	}
+// }
+
 func JsonPathEval(jsonData string, expression string) (*string, error) {
-	paths, err := jsonpath.ParsePaths(expression)
-	if err != nil {
-		return nil, err
-	}
-	newLogger.Debugf("jsonpath expression is [%v], data is [%v]", expression, jsonData)
+	fmt.Println("##########################################")
+	var jsondata interface{}
+	json.Unmarshal([]byte(jsonData), &jsondata)
 
-	eval, err := jsonpath.EvalPathsInBytes([]byte(jsonData), paths)
+	res, err := jsonpath.JsonPathLookup(jsondata, expression)
 	if err != nil {
-		newLogger.Infof("unable to evaluate jsonpath expression, error is [%v]", err)
-		return nil, err
+		return nil, errors.New(fmt.Sprintf("Error evaluating jsonpath expression[%v] on content [%v]", expression, jsonData))
 	}
+	strVar := res.(string)
+	newLogger.Debugf("jsonpath [%v] evaluated to value [%v]", expression, strVar)
+	fmt.Println("##########################################")
+	return &strVar, nil
 
-	for {
-		if result, ok := eval.Next(); ok {
-			//return after the first match
-			value := string(result.Value) //The value obtained will be encased in double quote characters, e.g. "USA" when the value happens to be USA.
-			//Trim the double quote suffix and prefix
-			value = strings.TrimPrefix(value, "\"")
-			value = strings.TrimSuffix(value, "\"")
-			newLogger.Debugf("jsonpath [%v] evaluated to value [%v]", expression, value)
-			return &value, nil // true -> show keys in pretty string
-		} else {
-			return nil, errors.New(fmt.Sprintf("Error evaluating jsonpath expression[%v] on content [%v]", expression, jsonData))
-		}
-	}
 }
